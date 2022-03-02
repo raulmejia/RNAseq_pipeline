6f912b0672a5e3f98a78b9a50b2b2bdf24d0d42c
raulmejia <r8a8m8p8@gmail.com>
2022-03-02 17:54:34 +0100

    waiting for the args code

--- Folder_containing_sequences_2_Quality_Control_graphs.sh
+++ Folder_containing_sequences_2_Quality_Control_graphs.sh
@@ -9,10 +9,11 @@
 # youroutputfolder=
 # yourlogsfolder=
 # inputfiles=
-#  # Change *.gz , *.fastq , according to your files extension
+#  # Change *.gz , *.fastq , according to your files extension (it also accepts bam, sam , etc )
 # you could add the argumen "-t 6" to fastqc to use 6 threads and speed up the process
 
 mkdir -p $youroutputfolder
 mkdir -p $yourlogsfolder # hint for choosing a name: youroutputfolder_logs
 
-fastqc *  -o &> $yourlogsfolder
+fastqc *.fastq.gz -t 6 -o $youroutputfolder &> $yourlogsfolder
+
